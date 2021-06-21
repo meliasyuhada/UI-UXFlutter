7afd213b0d17807a53f430bbf62f25a24fa8f929
Melia Syuhada <meliasyuhada5@gmail.com>
2021-06-21 11:32:51 +0700

    home po

--- ui_uxnews/lib/views/home_view.dart
+++ ui_uxnews/lib/views/home_view.dart
@@ -21,7 +21,7 @@ class HomeView extends StatelessWidget {
                   style: kNonActiveTabStyle,
                 ),
                 subtitle: Text(
-                  "Jessica Veranda",
+                  "Septi Melia",
                   textAlign: TextAlign.end,
                   style: kActiveTabStyle,
                 ),
@@ -31,7 +31,7 @@ class HomeView extends StatelessWidget {
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10.0),
                     image: DecorationImage(
-                      image: AssetImage("assets/ve.jpg"),
+                      image: AssetImage("assets/saya.jpg"),
                       fit: BoxFit.cover,
                     ),
                   ),
